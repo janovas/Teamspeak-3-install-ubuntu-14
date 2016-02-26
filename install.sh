50c37ced80b1df25de03ef0069a4cfb7a9c32c87
janovas <janovas@outlook.com>
2016-02-26 17:32:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ tar xzf teamspeak3-server_linux-amd64-3.0.11.4.tar.gz;
 sudo adduser --disabled-login teamspeak;
 sudo mv teamspeak3-server_linux-amd64 /home/teamspeak;
 sudo chown -R teamspeak:teamspeak /home/teamspeak;
-sudo ln -s /home/teamspeak/ts3server_startscript.sh /etc/init.d/teamspeak;
+sudo ln -s /home/teamspeak/teamspeak3-server_linux-amd64/ts3server_startscript.sh /etc/init.d/teamspeak;
 sudo update-rc.d teamspeak defaults;
 sudo service teamspeak start;
