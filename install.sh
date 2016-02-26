22cdc51c7050c9a445deb5dc92a0be2e100797a8
janovas <janovas@outlook.com>
2016-02-26 17:14:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ wget http://dl.4players.de/ts/releases/3.0.12.2/teamspeak3-server_linux_amd64-3.
 tar xzf teamspeak3-server_linux-amd64-3.0.12.2.tar.bz2;
 deluser teamspeak;
 sudo adduser --disabled-login teamspeak;
-sudo mv teamspeak3-server_linux-amd64 /usr/local/teamspeak;
-sudo chown -R teamspeak:teamspeak /usr/local/teamspeak;
-sudo ln -s /usr/local/teamspeak/ts3server_startscript.sh /etc/init.d/teamspeak;
+sudo mv teamspeak3-server_linux-amd64 /home/teamspeak;
+sudo chown -R teamspeak:teamspeak /home/teamspeak;
+sudo ln -s /home/teamspeak/ts3server_startscript.sh /etc/init.d/teamspeak;
 sudo update-rc.d teamspeak defaults;
 sudo service teamspeak start;
